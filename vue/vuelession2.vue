bd890c69722747e78abfd624406f0a3e215c0333
liyeace <11225757+liyeace@user.noreply.gitee.com>
2023-01-04 15:49:27 +0800

    feat: 创建react & vue

---
+++ vue/vuelession2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="cell-changed">
+    <mt-input
+      :id="data.column.field"
+      v-model="data[data.column.field]"
+      :disabled="isDisabled"
+    ></mt-input>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return { data: {}, isDisabled: false };
+  },
+  mounted() {
+    if (
+      this.data.column.field === "scheduleArea" ||
+      this.data.column.field === "associatedNumber" ||
+      this.data.column.field === "buyerOrgName" ||
+      this.data.column.field === "remark" ||
+      this.data.column.field === "saleOrder" ||
+      this.data.column.field === "saleOrderRowCode" ||
+      this.data.column.field === "processName" ||
+      this.data.column.field === "productCode" ||
+      this.data.column.field === "buyerOrder" ||
+      this.data.column.field === "buyerOrderRowCode" ||
+      this.data.column.field === "projectTextBatch" ||
+      this.data.column.field === "warehouseKeeper"
+    ) {
+      //计划区域 关联工单号
+      this.setDisabled();
+    }
+  },
+  methods: {
+    setDisabled() {
+      if (this.data.isEntry === "1") {
+        this.isDisabled = true;
+      }
+      if (this.data.isEntry === "2") {
+        this.isDisabled = false;
+      }
+    },
+  },
+};
+</script>
